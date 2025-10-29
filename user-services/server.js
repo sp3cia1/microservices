c8afc115cf1c77508d69b7ed32a10594699d423e
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-10-29 13:40:40 +0530

    Removed root files to focus on microservices and update user-services server configuration

--- user-services/server.js
+++ user-services/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 const authRoutes = require('./routes')
 const connectDb = require('./config/db')
@@ -12,6 +12,10 @@ connectDb();
 
 app.use(express.json())
 app.use('/api/auth', authRoutes)
+
+app.get('/', (req, res) => {
+  res.send('User Services is running')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
