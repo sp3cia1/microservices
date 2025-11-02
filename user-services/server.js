a7c729b60f46b70218ab716411a6ae7c9adfb3a9
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-11-03 01:08:23 +0530

    feat: implement RabbitMQ integration for user service with order consumption and notification

--- user-services/server.js
+++ user-services/server.js
@@ -6,16 +6,25 @@ const app = express()
 const PORT = 3000
 
 const authRoutes = require('./routes/authRoutes')
+const {connectRabbitMQ, consumeMessage} = require('./utils/rabbitMQ');
 const connectDb = require('./config/db')
 
 connectDb();
 
 app.use(express.json())
+async function notifyUser(order) {
+  console.log("User service received order:", order);
+}
 app.use('/api/auth', authRoutes)
 
 app.get('/', (req, res) => {
   res.send('User Services is running')
 })
+
+(async () => {
+  await connectRabbitMQ();
+  await consumeMessage('orderQueue', notifyUser);
+})();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
